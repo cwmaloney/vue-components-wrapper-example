95b6453e3174ef066d65aeb6cf3b3c78260ba4b8
Chris Maloney <cmaloney@euronetworldwide.com>
2018-03-02 12:04:14 -0600

    added a simple counter to demonstrate property binding

---
+++ src/components/SimpleCounter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="simple-counter">
+    <button v-on:click="increment">increment</button>
+    <button v-on:click="decrement">decrement</button>
+    {{counter}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'simple-counter',
+
+    props: {
+      'initialCounterValue': {type: Number, required: true}
+    },
+
+    data() {
+      return {
+        counter: this.initialCounterValue
+      };
+    },
+
+    methods: {
+      increment: function () {
+        this.counter += 1;
+        this.$emit('update', this.counter);
+      },
+      decrement: function () {
+        this.counter -= 1;
+        this.$emit('update', this.counter);
+      }
+    }
+  };
+</script>
+
+<style lang="css" scoped>
+  .simple-counter {
+    border: solid green;
+    padding: 4px;
+  }
+  button {
+    color: blue;
+  }
+</style>
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+//
+// This script registers the components with the Vue.s runtime.
+// By including this script in a page, all the components are registered with
+// the Vue.js runtime and can be used within a Vue component.
+//
+
 import HelloComponent from './components/HelloComponent.vue';
 import Hello2Component from './components/Hello2Component.vue';
+import SimpleCounter from './components/SimpleCounter.vue';
 
 import Vue from 'vue';
 
 Vue.component('hello-component', HelloComponent);
 Vue.component('hello2-component', Hello2Component);
+Vue.component('simple-counter', SimpleCounter);
