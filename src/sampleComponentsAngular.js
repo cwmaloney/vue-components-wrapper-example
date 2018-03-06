970740d69286c1dca4453a014b476fc54ea1bec1
Chris Maloney <cmaloney@euronetworldwide.com>
2018-03-06 15:23:38 -0600

    added Contact wrapper to demo binding objects fixed wrapper support for multiple Vue.js components in a page

--- src/sampleComponentsAngular.js
+++ src/sampleComponentsAngular.js
@@ -13,21 +13,18 @@ angular.module('componentWrappers', [])
 
       template: '<div>\n' +
                 '  <div ng-non-bindable>' +
-                '    <div id="wrapper">' +
-                '      <simple-counter' +
-                '        v-bind:initial-count="initialValueFromWrapper"' +
-                '        v-on:count-updated="onCountUpdated"' +
-                '        >' +
-                '      </simple-counter>' +
-                '    </div>' +
+                '    <simple-counter' +
+                '      v-bind:initial-count="initialValueFromWrapper"' +
+                '      v-on:count-updated="onCountUpdated">' +
+                '    </simple-counter>' +
                 '  </div>' +
-                '  Angular wrapper $scope value: {{count}}\n' +
+                '  Angular scope count: {{count}}\n' +
                 '</div>',
 
-      link: function (scope, $element) {
+      link: function (scope, elem) {
         // Create our root instance
         scope.vue = new Vue({
-          el: '#wrapper',
+          el: elem[0].querySelector('[ng-non-bindable]'),
           data: function () {
             return {
               initialValueFromWrapper: scope.count
@@ -35,7 +32,7 @@ angular.module('componentWrappers', [])
           },
           methods: {
             onCountUpdated: function (newCount) {
-              console.log('onCountUpdated' + newCount);
+              console.log('onCountUpdated=' + newCount);
               scope.$apply(function () {
                 scope.count = newCount;
               });
@@ -43,6 +40,50 @@ angular.module('componentWrappers', [])
           }
         });
         console.log("scope.count=" + scope.count)
+      }
+    }
+  })
+  .directive('simpleContactWrapper', function () {
+    return {
+      restrict: 'E', // match element only
+
+      scope: {
+        contact: '=initialContact'
+      },
+
+      template: '<div>\n' +
+                '  <div ng-non-bindable>' +
+                '    <simple-contact' +
+                '      v-bind:initial-contact="initialContactFromWrapper"' +
+                '      v-on:contact-updated="onContactUpdated"' +
+                '      v-on:cancel="onContactCancel">' +
+                '    </simple-contact>' +
+                '  </div>' +
+                '  Angular scope contact: {{contact}}\n' +
+                '</div>',
+
+      link: function (scope, elem) {
+        // Create our root instance
+        scope.vue = new Vue({
+          el: elem[0].querySelector('[ng-non-bindable]'),
+          data: function () {
+            return {
+              initialContactFromWrapper: scope.contact
+            }
+          },
+          methods: {
+            onContactUpdated: function (newContact) {
+              console.log('onContactUpdated=' + JSON.stringify(newContact));
+              scope.$apply(function () {
+                scope.contact = newContact;
+              });
+            },
+            onContactCancel: function () {
+              console.log('onContactCancel');
+            }
+          }
+        });
+        console.log("scope.contact=" + JSON.stringify(scope.contact));
       }
     }
   });
